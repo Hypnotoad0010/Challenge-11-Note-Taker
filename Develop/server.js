ef9e4879b072baff733ddde5cd43105f13fd35fb
Isaac Harris <IsaacHphotography@gmail.com>
2022-09-18 16:29:03 -0600

    Moved routes location

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,20 @@
-//creating express server
-const app = express();
-
 const express= require('express');
 const htmlRoute = require('./routes/htmlRoutes');
 const apiRoute = require('./routes/htmlRoutes');
 
+const app = express();
+//Port set to 3001
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
+//Takes server to route files
 app.use('/', htmlRoutes);
 app.use('api', apiRoutes);
 
+//Listener function
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
