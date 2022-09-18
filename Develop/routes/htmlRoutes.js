084e85aeb3d720fd3e4edfb8d62bbbcd1af64d3c
Isaac Harris <IsaacHphotography@gmail.com>
2022-09-18 15:30:46 -0600

    Added get routes to html and created routes folders

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+//Loads modules
+const path = require('path');
+const router = require('express').Router();
+
+//Get request for HTML
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+//Get request for Notes
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+module.exports = router;
